7ccc2e98abb3f5880b3594e3cb61cb66e25e6992
sawyerrick <1103031078@qq.com>
2023-09-18 22:06:47 +0800

    feat: verify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,11 @@ export default function Home() {
     e.preventDefault()
     if (nameRef.current?.value.trim() == '') {
       alert("Name is required.")
+      return
     }
     if (inputRef.current?.value.trim() == '') {
       alert("Comment is required.")
+      return
     }
 
     const comment = {
@@ -19,6 +21,8 @@ export default function Home() {
       name: nameRef.current?.value,
       emailRef: emailRef.current?.value
     }
+
+    // call api
 
     console.log("Submit comment:", JSON.stringify(comment))
   }
@@ -28,14 +32,14 @@ export default function Home() {
     <main>
       <form onSubmit={handleSubmit} className="flex flex-col p-3 gap-3">
         <div className="flex gap-3 grow min-w-0">
-          <input ref={nameRef} className="border border-black grow p-3 rounded-sm min-w-0
+          <input ref={nameRef} className="h-12 border border-black grow p-3 rounded-sm min-w-0
           focus:ring outline-none ring-black"
             type="text" placeholder="Name" />
-          <input ref={emailRef} className="border border-black grow p-3 rounded-sm min-w-0
+          <input ref={emailRef} className="h-12 border border-black grow p-3 rounded-sm min-w-0
           focus:ring outline-none ring-black"
             type="text" placeholder="Email (Optional)" />
         </div>
-        <textarea ref={inputRef} className="border border-black grow p-3 rounded-sm focus:ring outline-none ring-black"
+        <textarea ref={inputRef} className="h-24 border border-black grow p-3 rounded-sm focus:ring outline-none ring-black"
           placeholder="Say someting..." />
         <button className="self-start p-3 font-bold bg-gray-300 
           hover:bg-gray-400 focus:bg-gray-400 active:bg-gray-500 rounded-sm focus:ring outline-none ring-black">
